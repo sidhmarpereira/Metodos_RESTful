c9f85e3e5eab25bad6ea9f9eb38213eca18e4031
Sidhmar <sidhmarpereiraKesus@outloo.com>
2025-06-20 18:25:56 -0300

    Fred Crud ok

--- index.js
+++ index.js
@@ -37,6 +37,53 @@ return res.status(201).json(newCustomer)
 
 });
 
+server.put("/customers/:id", (req, res) => {
+const id = parseInt(req.params.id);
+const { name, site} = req.body;
+
+const index = customers.findIndex(item => item.id === id);
+const status = index >= 0 ? 200 : 400;
+
+if (index >= 0){
+customers[index] = { id: parseInt(id), name, site};
+
+}
+
+return res.status(status).json(customers[index]);
+});
+
+server.delete("/customers/:id",(req, res) => {
+const id = parseInt(req.params.id);
+const index = customers.findIndex(item => item.id === id);
+const status = index >= 0 ? 200 : 404;
+
+if (index >= 0){
+  customers.splice(index, 1);
+
+}
+
+return res.status(status).json();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 server.listen(3000);
 
